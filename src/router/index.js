cdc0cc893eb9627ab32a8ce96af94f0eba71217e
aflori <f.aurelien@yahoo.fr>
2024-03-04 14:53:21 +0100

    3 - menu redirection target

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Products from '../views/Products.vue'
+import Product from '../views/Products.vue'
+import LogIn from '../views/LogIn.vue'
+import Command from '../views/Command.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/products',
+        name: 'product list',
+        component: Products
+    },
+    {
+        path: '/product',
+        name: 'product',
+        component: Product
+    },
+    {
+        path: '/login',
+        name: 'Log In',
+        component: LogIn
+    },
+    {
+        path: '/command',
+        name: 'command',
+        component: Command
     },
   ]
 })
