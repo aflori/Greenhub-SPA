37e457ebe34661f7f8d4fc8e173a45679c7c1e02
aflori <f.aurelien@yahoo.fr>
2024-03-04 12:09:46 +0100

    #3 - logo redirection

--- src/components/molecules/LogoData.vue
+++ src/components/molecules/LogoData.vue
@@ -1,10 +1,11 @@
 <script setup>
     import Logo_greenhub from "../icons/Logo_greenhub.vue"
+    import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <div class="grid grid-cols-2 my-2">
+    <RouterLink to="/" class="grid grid-cols-2 my-2">
         <Logo_greenhub class="h-16" />
         <span class="font-serif my-auto font-extrabold text-2xl text-info"> Greenhub </span>
-    </div>
+    </RouterLink>
 </template>
