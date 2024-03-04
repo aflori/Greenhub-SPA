5b1bc417a15d909c20d79760c27ea8b1741383e0
aflori <f.aurelien@yahoo.fr>
2024-03-04 10:49:16 +0100

    prune default content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
     <button class="btn btn-primary">Hello daisyui</button>
   </main>
 </template>
