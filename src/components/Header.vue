07c2ec2d0bd124a1cfd4b63d110efedd69c7ac8e
aflori <f.aurelien@yahoo.fr>
2024-03-04 11:50:03 +0100

    #3 first header molecules with minimal style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,13 @@
 <script setup>
     import Logo_greenhub from "./icons/Logo_greenhub.vue"
+    import LogoData from "./molecules/LogoData.vue"
 </script>
 
 <template>
     <header class="bg-accent flex flex-row justify-evenly">
-        <Logo_greenhub />
-        <Logo_greenhub />
-        <Logo_greenhub />
-        <Logo_greenhub />
+        <LogoData />
+        <Logo_greenhub class="h-10"/>
+        <Logo_greenhub class="h-10"/>
+        <Logo_greenhub class="h-10"/>
     </header>
 </template>
---
+++ src/components/molecules/LogoData.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import Logo_greenhub from "../icons/Logo_greenhub.vue"
+</script>
+
+<template>
+    <div class="grid grid-cols-2">
+        <Logo_greenhub />
+        <span class="font-serif my-auto"> Greenhub </span>
+    </div>
+</template>
