8e1486e11dd45e40e51c32053a43a3fc1b02b668
aflori <f.aurelien@yahoo.fr>
2024-03-04 16:44:13 +0100

    #5 - created product component and use it

---
+++ src/components/molecules/BasicProductCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+    const props = defineProps({
+        id: Number,
+        title: String, //--
+        price: String,
+        categories: Array,
+        description: String, //--
+        image: String, // --
+    });
+    console.log(props.urlName);
+    // console.log(route);
+</script>
+
+<template>
+    <div class="card w-96 bg-base-100 shadow-xl p-4 m-2 bg-primary">
+        <h2 class="card-title">
+            {{ props.title }}
+            <!-- <div class="badge badge-secondary">NEW</div> -->
+        </h2>
+        <figure>
+            <img :src="props.image" :alt="props.title" />
+        </figure>
+        <div class="card-body">
+            <p>{{ props.description }}</p>
+            <div class="card-actions justify-end">
+                <div class="badge badge-outline" v-for="category in categories">{{category}}</div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,10 +1,11 @@
 <script setup>
-    const listArticles = [
+    import BasicProductCard from "../components/molecules/BasicProductCard.vue";
+    const products = [
     {
         id: 1,
         title: "Blue shoes!",
         price: '32.5 €',
-        category: ['toto'],
+        categories: ['toto'],
         description: 'If a dog chews shoes whose shoes does he choose?',
         image: 'https://images.pexels.com/photos/19090/pexels-photo.jpg'
     },
@@ -12,7 +13,7 @@
         id: 2,
         title: "White shoes!",
         price: '55.5 €',
-        category: ['titi'],
+        categories: ['tata', 'titi'],
         description: 'If a cat chews shoes whose shoes does he choose?',
         image: 'https://images.pexels.com/photos/2529147/pexels-photo-2529147.jpeg'
     }];
@@ -20,22 +21,7 @@
 </script>
 
 <template>
-    <main>
-        <div class="card w-96 bg-base-100 shadow-xl p-4 bg-primary">
-            <h2 class="card-title">
-                Shoes!
-                <div class="badge badge-secondary">NEW</div>
-            </h2>
-            <figure>
-                <img src="https://images.pexels.com/photos/19090/pexels-photo.jpg" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <div class="badge badge-outline">Fashion</div>
-                    <div class="badge badge-outline">Products</div>
-                </div>
-            </div>
-        </div>
+    <main class="flex flex-row flex-wrap">
+        <BasicProductCard v-bind="product" v-for="product in products"/>
     </main>
 </template>
