9969bd88747981e410e3dd5876cd36b4d748dee3
aflori <f.aurelien@yahoo.fr>
2024-03-04 14:51:53 +0100

    #3 - more CSS style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,20 @@
 <script setup>
     import Logo_greenhub from "./icons/Logo_greenhub.vue"
     import LogoData from "./molecules/LogoData.vue"
+    import MenuLink from "./molecules/MenuLink.vue"
+    let toto = "titi";
 </script>
 
 <template>
-    <header class="bg-accent flex flex-row justify-evenly flex-wrap">
-        <LogoData />
-        <!-- <Logo_greenhub class="h-10"/> -->
-        <!-- <Logo_greenhub class="h-10"/> -->
-        <!-- <Logo_greenhub class="h-10"/> -->
+    <header class="bg-accent">
+        <div id="logos" class="flex flex-row justify-evenly container">
+            <LogoData />
+            <!-- <Logo_greenhub class="h-10"/> -->
+            <!-- <Logo_greenhub class="h-10"/> -->
+            <!-- <Logo_greenhub class="h-10"/> -->
+        </div>
     </header>
+    <div id="menu" class="flex flex-row justify-evenly container">
+        <MenuLink :url="toto" text="abcd"/>
+    </div>
 </template>
--- src/components/molecules/LogoData.vue
+++ src/components/molecules/LogoData.vue
@@ -6,6 +6,6 @@
 <template>
     <RouterLink to="/" class="grid grid-cols-2 my-2">
         <Logo_greenhub class="h-16" />
-        <span class="font-serif my-auto font-extrabold text-2xl text-info max-sm:invisible"> Greenhub </span>
+        <span class="font-serif my-auto font-extrabold text-2xl text-info max-sm:invisible max-sm:text-base max-sm:font-extralight"> Greenhub </span>
     </RouterLink>
 </template>
