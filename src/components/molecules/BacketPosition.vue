24c053e715665fd2eb9180af2d17d4ec010aebdb
aflori <f.aurelien@yahoo.fr>
2024-03-06 14:49:29 +0100

    #15 - dynamic step of command

--- src/components/molecules/BacketPosition.vue
+++ src/components/molecules/BacketPosition.vue
@@ -1,7 +1,11 @@
 <script setup>
+    import { defineProps, computed } from 'vue';
 
+    const props = defineProps(["text", "stepNumber", "position"])
+    const stepIsDone = computed(() => props.stepNumber >= props.position);
+    const stepToDo = computed(() => props.stepNumber <= props.position);
 </script>
 
 <template>
-    <li class="step step-neutral step-primary">panier</li>
+    <li :class="{'step': true, 'step-neutral': stepIsDone, 'step-primary': stepToDo}">{{ text }}</li>
 </template>
--- src/views/Command.vue
+++ src/views/Command.vue
@@ -1,17 +1,23 @@
 <script setup>
     import { ref } from 'vue';
     import ListElement from "@/components/molecules/BacketPosition.vue"
+
     const position = ref(1);
+    const texts = [
+        "panier",
+        "livraisons",
+        "paiement",
+        "confirmation"
+    ];
 </script>
 
 <template>
     <main>
         <button class="btn" @click="position++">next</button>
         <button class="btn" @click="position--">previous</button>
-        <p>{{position}}</p>
         <ul class="steps steps-vertical lg:steps-horizontal">
-            <ListElement />
-            <!-- <li class="step step-neutral step-primary">panier</li> -->
+            <ListElement :text="text" :stepNumber="index+1" :position="position" v-for="(text,index) in texts"/>
+            <!-- <li class="step step-neutral step-primary">panier</li>
             <li class="step step-neutral">livraisons</li>
             <li class="step step-neutral">paiement</li>
             <li class="step step-neutral">confirmation</li>
@@ -21,7 +27,7 @@
             <li class="step step-neutral">test</li>
             <li class="step step-neutral">test</li>
             <li class="step step-neutral">test</li>
-            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li> -->
         </ul>
     </main>
 </template>
