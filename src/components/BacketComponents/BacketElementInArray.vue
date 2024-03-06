327550c87d7d609c442e94779e866c52b7c70179
aflori <f.aurelien@yahoo.fr>
2024-03-06 16:39:35 +0100

    #5 - better CSS

--- src/components/BacketComponents/BacketElementInArray.vue
+++ src/components/BacketComponents/BacketElementInArray.vue
@@ -1,5 +1,7 @@
 <script setup>
     import { defineProps } from "vue";
+    import IconModify from "@/components/icons/Modify.vue";
+    import { useBacketStore } from "@/stores/backet.js";
 
     const props = defineProps(["products"]);
 </script>
@@ -7,7 +9,7 @@
 <template>
     <tr>
         <td>{{ products.product.title }}</td>
-        <td>{{ products.quantity}}</td>
+        <td class="flex"> <span class="mx-2">{{ products.quantity}}</span> <IconModify /> </td>
         <td>{{ products.unitPrice}}</td>
         <td>{{ products.totalPrice}}</td>
     </tr>
--- src/components/molecules/BasicProductCard.vue
+++ src/components/molecules/BasicProductCard.vue
@@ -15,18 +15,17 @@
 </script>
 
 <template>
-    <div class="card w-96 bg-base-100 shadow-xl p-4 m-2 bg-primary">
+    <div class="card w-96 bg-base-100 shadow-xl p-4 m-2 bg-primary w-80 mx-auto">
         <RouterLink :to="{name: 'product', params: {id: product.id}}" class="">
-            <h2 class="card-title">
+            <h2 class="card-title h-24 text-center mx-auto w-fit">
                 {{ product.title }}
-                <!-- <div class="badge badge-secondary">NEW</div> -->
             </h2>
             <figure>
-                <img :src="product.image" :alt="product.title" />
+                <img :src="product.image" :alt="product.title" class="w-48 h-40" />
             </figure>
         </RouterLink>
-        <div class="card-body">
-            <p>{{ product.description }}</p>
+        <div class="card-body p-4">
+            <p class="leading-6">{{ product.description }}</p>
             <div class="card-actions justify-end flex">
                 <p>{{product.price}}</p>
                 <div class="badge badge-outline" v-for="category in product.categories">{{category}}</div>
