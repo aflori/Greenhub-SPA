29eae4b02f34584a8fb869038a49ac42db60cab2
aflori <f.aurelien@yahoo.fr>
2024-03-06 15:48:53 +0100

    #15 - static page (if product 3 has been placed on backets)

--- src/components/BacketComponents/BacketComposition.vue
+++ src/components/BacketComponents/BacketComposition.vue
@@ -2,9 +2,30 @@
     import { useBacketStore } from "@/stores/backet.js";
 
     const backetData = useBacketStore();
-    const products = backetData.listProductInBacket; //read-only
+    const products = backetData.listProductInBacket[3]; //read-only - to modify with methods
+
 </script>
 
 <template>
-    <p> successfully imported </p>
+    <div class="overflow-x-auto">
+          <table class="table">
+            <!-- head -->
+            <thead>
+              <tr>
+                <th>produit</th>
+                <th>quantité</th>
+                <th>prix unitaire</th>
+                <th>prix total</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ products.product.title }}</td>
+                    <td>{{ products.quantity}}</td>
+                    <td>{{ products.unitPrice}}</td>
+                    <td>{{ products.totalPrice}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
