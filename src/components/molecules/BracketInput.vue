763b0a51a77c7a7ae78dd0b594e3d0f709980615
aflori <f.aurelien@yahoo.fr>
2024-03-06 09:50:11 +0100

    #6 - input for number for buying a specific quantity

---
+++ src/components/molecules/BracketInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { ref } from "vue";
+    const numberOfProduct = ref(1);
+
+    function updateValue(event) {
+        const tag = event.target
+        const newValue = tag.value;
+
+        if (newValue < 1) {
+            tag.value = numberOfProduct.value;
+            return ;
+        }
+        else {
+            numberOfProduct.value = Number(newValue);
+        }
+
+
+    }
+</script>
+
+<template>
+    <label class="form-control w-full max-w-sm flex flex-row">
+        <span class="label-text"></span>
+        <input type="number" min="1" :value="numberOfProduct" @input="updateValue" class="input input-bordered w-full max-w-xs" />
+    </label>
+</template>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { defineProps, ref } from "vue";
     import { useProductListStore } from "@/stores/listProduct.js";
-
+    import BraketInput from "@/components/molecules/BracketInput.vue";
     function getIdOnGoodType(propsParam) {
         return Number(propsParam.id);
     }
@@ -15,14 +15,27 @@
     const productId = getIdOnGoodType(props)
     store.getSingleProduct(productId)
         .then((productData) => {
-            product.value = productData
-        });
+        product.value = productData
+    });
 
 </script>
 
 <template>
     <main>
         <!-- the v-if test the product chargement by looking into its main attribute -->
-        <p v-if="product['id'] !== undefined"> {{ product.description }} </p>
+        <div v-if="product['id'] !== undefined">
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+              <figure><img :src="product.image" alt="Album"/></figure>
+              <div class="card-body">
+                <h2 class="card-title">{{ product.title }}</h2>
+                <p> {{ product.description }}</p>
+                <div class="card-actions justify-between">
+                    <div> <p> {{ product.price }} </p> </div>
+                    <BraketInput />
+                    <button class="btn btn-primary">Acheter</button>
+                </div>
+              </div>
+            </div>
+        </div>
     </main>
 </template>
