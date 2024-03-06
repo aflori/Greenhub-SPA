e9d1af1b03b4518578d29009ae63335e77c5dc17
aflori <f.aurelien@yahoo.fr>
2024-03-06 14:28:55 +0100

    #15 - step of command

---
+++ src/components/molecules/BacketPosition.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <li class="step step-neutral step-primary">panier</li>
+</template>
--- src/views/Command.vue
+++ src/views/Command.vue
@@ -1,13 +1,27 @@
 <script setup>
+    import { ref } from 'vue';
+    import ListElement from "@/components/molecules/BacketPosition.vue"
+    const position = ref(1);
 </script>
 
 <template>
     <main>
+        <button class="btn" @click="position++">next</button>
+        <button class="btn" @click="position--">previous</button>
+        <p>{{position}}</p>
         <ul class="steps steps-vertical lg:steps-horizontal">
-            <li class="step step-neutral step-primary">panier</li>
+            <ListElement />
+            <!-- <li class="step step-neutral step-primary">panier</li> -->
             <li class="step step-neutral">livraisons</li>
             <li class="step step-neutral">paiement</li>
             <li class="step step-neutral">confirmation</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
+            <li class="step step-neutral">test</li>
         </ul>
     </main>
 </template>
