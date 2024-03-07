4f598e7384a28c5a2d99daa540b295b3f46ed5a1
aflori <f.aurelien@yahoo.fr>
2024-03-07 10:10:42 +0100

    other responsive addition

--- src/components/BacketComponents/BacketElementInArray.vue
+++ src/components/BacketComponents/BacketElementInArray.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { defineProps } from "vue";
+    import { defineProps, ref } from "vue";
     import IconModify from "@/components/icons/Modify.vue";
     import { useBacketStore } from "@/stores/backet.js";
 
@@ -7,8 +7,15 @@
 </script>
 
 <template>
-    <tr>
-        <td> {{ products.product.title }} </td>
+    <tr class="max-w-full">
+        <td class="custom-max-w ">
+            <!-- daisyUI component for showing full title or not // section checkbox -->
+            <label class="swap">
+                <input type="checkbox" />
+                <div class="swap-off custom-max-w truncate my-auto"> {{ products.product.title }} </div>
+                <div class="swap-on"> {{ products.product.title }} </div>
+            </label>
+        </td>
         <td> <div class="grid grid-cols-2">
             <span class="mx-2">{{ products.quantity}}</span> <IconModify />
         </div> </td>
@@ -16,3 +23,9 @@
         <td>{{ products.totalPrice}}</td>
     </tr>
 </template>
+
+<style scoped>
+    .custom-max-w {
+        max-width: 29vw;
+    }
+</style>
