2a153fd621fd5a496c908b96071ef470ffc7c75d
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-08 10:40:26 +0100

    #15 - second part of paiement

--- src/views/Command.vue
+++ src/views/Command.vue
@@ -1,7 +1,8 @@
 <script setup>
     import { ref } from 'vue';
     import ListElement from "@/components/BacketComponents/BacketPosition.vue";
-    import BacketComposition from "@/components/BacketComponents/BacketComposition.vue"
+    import BacketComposition from "@/components/BacketComponents/BacketComposition.vue";
+    import BacketAdressStep from "@/components/BacketComponents/BacketAdressStep.vue";
 
     const position = ref(1);
     const texts = [
@@ -14,8 +15,8 @@
 
 <template>
     <main>
-        <BacketComposition @next-step="position++" v-if="position===1" />
-        
+        <BacketComposition @next-step="position++" v-if="position==1" />
+        <BacketAdressStep v-else-if="position==2" />
         <ul class="steps steps-vertical lg:steps-horizontal">
             <ListElement :text="text" :stepNumber="index+1" :position="position" v-for="(text,index) in texts"/>
         </ul>
