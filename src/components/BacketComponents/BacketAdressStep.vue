e3d9f42252ba8b2525079bdf4cf40b35c19969b2
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-08 11:44:55 +0100

    #15 - navigation button on second step

--- src/components/BacketComponents/BacketAdressStep.vue
+++ src/components/BacketComponents/BacketAdressStep.vue
@@ -1,9 +1,24 @@
 <script setup>
-    
+    import { ref,  defineEmits } from 'vue';
+
+    const canSubmitForm = ref(false);
+    const emit = defineEmits([ 'next-step' ]);
+
+    function emitIfFormIsValid() {
+        if(canSubmitForm.value) {
+            emit("next-step")
+        }
+    }
 </script>
 
 <template>
-    <div>
-         test
-    </div>
+    <form>
+
+        <button type="submit" class="btn" @click.prevent="emitIfFormIsValid()">
+            valider
+        </button>
+        <button type="reset" class="btn" @click="$emit('prev-step')">
+            retour
+        </button>
+    </form>
 </template>
