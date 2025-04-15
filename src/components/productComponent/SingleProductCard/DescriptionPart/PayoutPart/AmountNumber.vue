0e8be563eeb42cdbf4f3716e442b8697f7a95add
aflori <f.aurelien@yahoo.fr>
2025-04-15 17:04:38 +0200

    completed visualization of amount component

--- src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/AmountNumber.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/AmountNumber.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
+import Add_icon from '@/components/icons/Add_icon.vue';
+import LessIcon from '@/components/icons/LessIcon.vue'; 
 
 const props = defineProps({
     amount: Number
 })
+
+const value = ref(props.amount)
 </script>
 
 <template>
-    <p> amount !</p>
+    <div class="flex flex-row flex_wrap">
+        <LessIcon class="cursor-pointer"/>
+        <input type="text" v-model="value" min="1" class="max-w-8 px-1 text-center border border-black rounded-sm bg-[#B8BCC8]"/>
+        <Add_icon class="cursor-pointer"/>
+    </div>
 </template>
 
 <style scoped>
