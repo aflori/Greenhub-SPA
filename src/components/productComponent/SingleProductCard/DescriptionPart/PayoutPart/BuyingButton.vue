0990f90e623eda96bb778bcb93b1e4e96795f786
aflori <f.aurelien@yahoo.fr>
2025-04-15 21:35:38 +0200

    button CSS

--- src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/BuyingButton.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/BuyingButton.vue
@@ -6,12 +6,26 @@ const propos = defineProps({
     productId: String,
 })
 
+const button_color = "white"
 
 </script>
 
 <template>
-    <BuyButtonLogo />
+    <button class="flex flew-row bg-[#1E561E] rounded-md">
+        <BuyButtonLogo :color="button_color" class="my-auto ml-1"/>
+        <div class="mx-2">
+            <span class="span"> ACHAT </span>
+        </div>
+    </button>
 </template>
 
 <style scoped>
+.span {
+    text-align: center;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 13px;
+    font-weight: bold;
+    color: white;
+
+}
 </style>
