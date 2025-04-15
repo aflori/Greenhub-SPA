ae075a7a02f7d09c59cee5b28c1eb1e8cc5339ad
aflori <f.aurelien@yahoo.fr>
2025-04-15 17:54:43 +0200

    input event checker

--- src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/AmountNumber.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/AmountNumber.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, defineEmits, ref } from 'vue';
+import { defineProps, defineEmits, ref, watch } from 'vue';
 import Add_icon from '@/components/icons/Add_icon.vue';
 import LessIcon from '@/components/icons/LessIcon.vue'; 
 
@@ -7,14 +7,33 @@ const props = defineProps({
     amount: Number
 })
 
-const value = ref(props.amount)
+const localAmount = ref(props.amount)
+
+watch(localAmount, (newValue, prevValue) => {
+    if(newValue === "") {
+        newValue = 0
+    }
+
+    if(typeof(newValue) === "string") {
+        newValue = parseInt(newValue)
+    }
+
+    if (newValue === NaN) {
+        localAmount.value = prevValue;
+        return ;
+    }
+
+    if (newValue !== prevValue) {
+        localAmount.value = newValue
+    }
+})
 </script>
 
 <template>
     <div class="flex flex-row flex_wrap">
-        <LessIcon class="cursor-pointer" @click="value--" />
-        <input type="text" v-model="value" min="1" class="max-w-8 px-1 text-center border border-black rounded-sm bg-[#B8BCC8]"/>
-        <Add_icon class="cursor-pointer" @click="value++"/>
+        <LessIcon class="cursor-pointer" @click="localAmount--" />
+        <input type="text" v-model="localAmount" min="1" class="max-w-10 px-1 text-center border border-black rounded-sm bg-[#B8BCC8]"/>
+        <Add_icon class="cursor-pointer" @click="localAmount++"/>
     </div>
 </template>
 
