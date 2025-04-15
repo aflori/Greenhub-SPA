b728c7466040768e9f0211bf61684087415afbf2
aflori <f.aurelien@yahoo.fr>
2025-04-15 09:43:21 +0200

    added labels image printing

--- src/components/productComponent/SingleProductCard/DescriptionPart.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart.vue
@@ -11,7 +11,9 @@
 <template>
     <div class="flex flex-col items-center max-w-96">
         <p class="p"> {{ description }}</p>
-        <h2> test 2 </h2>
+        <div class="flex flex_row flex-wrap justify-center max-sm:hidden">
+            <img :src="label_image" v-for="label_image in labels" class="img"/>
+        </div>
     </div>
 </template>
 
@@ -21,5 +23,9 @@
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     font-size: 15px;
     line-height: 25px;
+}
+.img {
+    width: 35px;
+    height: 35px;
 }
 </style>
