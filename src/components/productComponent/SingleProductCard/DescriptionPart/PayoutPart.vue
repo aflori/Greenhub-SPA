24e61c97d9e92975d6b6311b8af7700c2f512b16
aflori <f.aurelien@yahoo.fr>
2025-04-15 11:55:04 +0200

    added amount component

--- src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart.vue
@@ -1,16 +1,22 @@
 <script setup>
-    import { defineProps } from 'vue';
+    import { defineProps, ref } from 'vue';
+    import AmountNumber from './PayoutPart/AmountNumber.vue';
 
     const props = defineProps({
         price: String,
         labels: Array
     })
+
+    const number_buyed = ref(1)
 </script>
 
 <template>
-    <h3> payout ! </h3>
+    <div class="sm:flex sm:flex-row sm:flex-wrap sm:justify-evenly">
+        <p class="m-2"> {{ price }}</p>
+        <AmountNumber :amount="number_buyed" class="m-2" />
+        <!-- <BuyingButtun class="m-2" /> -->
+    </div>
 </template>
 
 <style scoped>
-
 </style>
---
+++ src/components/productComponent/SingleProductCard/DescriptionPart/PayoutPart/AmountNumber.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    amount: Number
+})
+</script>
+
+<template>
+    <p> amount !</p>
+</template>
+
+<style scoped>
+</style>
