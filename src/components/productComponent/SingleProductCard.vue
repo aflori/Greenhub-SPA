3608eb8ccb3a18d51cb459cf4dc7fc5446b8a093
aflori <f.aurelien@yahoo.fr>
2025-04-15 09:03:07 +0200

    product card structure

--- src/components/productComponent/SingleProductCard.vue
+++ src/components/productComponent/SingleProductCard.vue
@@ -12,6 +12,11 @@
     </div>
     <div v-else>
         <h1 class="h1"> {{ product.title }} </h1>
+        <div class="flex flex-row flex-wrap justify-evenly m-3">
+            <img :src="product.image" class="max-w-96 max-h-96" />
+            <!-- <DescriptionPart :description="Object.description" :labels="[]" /> -->
+        </div>
+        <!-- <Payout :price="product.price" /> -->
     </div>
 </template>
 
