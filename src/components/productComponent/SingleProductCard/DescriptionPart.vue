8a8754168b535cb79a182bb75731064561f0d2f7
aflori <f.aurelien@yahoo.fr>
2025-04-15 09:23:26 +0200

    adding description

--- src/components/productComponent/SingleProductCard/DescriptionPart.vue
+++ src/components/productComponent/SingleProductCard/DescriptionPart.vue
@@ -9,9 +9,17 @@
 </script>
 
 <template>
-    <h2> hello world !</h2>
+    <div class="flex flex-col items-center max-w-96">
+        <p class="p"> {{ description }}</p>
+        <h2> test 2 </h2>
+    </div>
 </template>
 
 <style scoped>
-
+.p {
+    text-align: justify;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 15px;
+    line-height: 25px;
+}
 </style>
