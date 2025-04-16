d01f135809524466c575de9c801772b6026302a8
aflori <f.aurelien@yahoo.fr>
2025-04-16 19:19:18 +0200

    handling note change event

--- src/components/productComponent/CommentsOnProduct/NewCommentPost.vue
+++ src/components/productComponent/CommentsOnProduct/NewCommentPost.vue
@@ -4,12 +4,16 @@ import { ref } from 'vue';
 
 const note = ref(0)
 const commentContent = ref("")
+
+function updateNote(newNote) {
+    note.value = newNote
+}
 </script>
 
 <template>
     <form>
         <h3 class="text-center"> poster une nouvelle note </h3>
-        <NoteInStar />
+        <NoteInStar @note-change="updateNote"/>
         <!-- <CommentTextContent /> -->
         <!-- <ButtonAddComment @click.prevent="addComment()" /> -->
     </form>
