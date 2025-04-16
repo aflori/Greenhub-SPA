f2597def7eb9e1bba5ef2e9426472487b139fe7d
aflori <f.aurelien@yahoo.fr>
2025-04-16 19:46:01 +0200

    comment text style

--- src/components/productComponent/CommentsOnProduct/NewCommentPost/CommentTextContent.vue
+++ src/components/productComponent/CommentsOnProduct/NewCommentPost/CommentTextContent.vue
@@ -1,10 +1,28 @@
 <script setup>
+import { ref } from 'vue';
+
+const textContent = ref("")
 
 </script>
 
 <template>
-
+    <div class="flex flex-col items-center md:flex-row justify-between">
+        <div>
+            <span> Explications :</span>
+        </div>
+        <div class="div">
+            <textarea class="bg-[#BEBEBE] w-full h-full  " v-model="textContent" placeholder="ton commentaire">
+            </textarea>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.div {
+    border-color: black;
+    border-style: solid;
+    border-width: 1px;
+    width: 564px;
+    height: 235px;
+}
 </style>
