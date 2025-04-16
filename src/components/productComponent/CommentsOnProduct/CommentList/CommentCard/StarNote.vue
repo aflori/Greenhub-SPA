8b17cb7d4859ac772cfb5764076f59d9e4cd607b
aflori <f.aurelien@yahoo.fr>
2025-04-16 21:37:23 +0200

    end of basic structure of comment card

--- src/components/productComponent/CommentsOnProduct/CommentList/CommentCard/StarNote.vue
+++ src/components/productComponent/CommentsOnProduct/CommentList/CommentCard/StarNote.vue
@@ -1,12 +1,21 @@
 <script setup>
+import Star_fill from '@/components/icons/Star_fill.vue';
+import Star from '@/components/icons/Star.vue';
 
 const props = defineProps({
     note: Number
 })
+
+const possibleNotes= [1,2,3,4,5]
 </script>
 
 <template>
-
+    <div>
+        <div v-for="i in possibleNotes">
+            <Star_fill v-if="i<=note" />
+            <Star v-else />
+        </div>
+    </div>
 </template>
 
 <style scoped>
