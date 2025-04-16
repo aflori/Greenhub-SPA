4dca93365260f37c4740c975ad84e0983b5d8dc2
aflori <f.aurelien@yahoo.fr>
2025-04-16 18:51:49 +0200

    comments section structure

--- src/components/productComponent/CommentsOnProduct.vue
+++ src/components/productComponent/CommentsOnProduct.vue
@@ -7,14 +7,15 @@ const props = defineProps({ comments: Array | null})
 
 <template>
     <div v-if="comments===null"></div>
-    <div v-else class="div">
-
+    <div v-else class="div rounded-md">
+        <!-- <CommentList /> -->
+        <!-- <NewCommentPost /> -->
     </div>
 </template>
 
 <style scoped>
 .div {
     background-color: rgba(249, 167, 4, 0.3);
-    min-height: 10px;
+    /* min-height: 100px; */
 }
 </style>
