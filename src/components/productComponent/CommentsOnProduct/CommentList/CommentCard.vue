ba0e0db0af21abfb9c9b24d750fe7607b20e79fe
aflori <f.aurelien@yahoo.fr>
2025-04-16 21:54:24 +0200

    comment card style

--- src/components/productComponent/CommentsOnProduct/CommentList/CommentCard.vue
+++ src/components/productComponent/CommentsOnProduct/CommentList/CommentCard.vue
@@ -7,13 +7,18 @@
 </script>
 
 <template>
-    <div>
-        <IntroField :pseudo="comment.pseudo" :note="comment.note" />
-        <p>
+    <div class="flex flex-col md:flex-row md:mx-20 mx-5 pt-10 ">
+        <IntroField class="self-center":pseudo="comment.pseudo" :note="comment.note" />
+        <div class="ml-14"> <p class="p">
             {{ comment.comment }}
-        </p>
+        </p> </div>
     </div>
 </template>
 
 <style scoped>
+.p {
+  text-align: justify;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 14px;
+}
 </style>
